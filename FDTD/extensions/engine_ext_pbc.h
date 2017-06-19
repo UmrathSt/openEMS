083ec6cca2528dc03a8bf7d73460ec9ae4686ffe
Stefan <Stefan.Umrath@dlr.de>
2017-06-19 17:47:52 +0200

    Creating PBC engine extension header file

--- FDTD/extensions/engine_ext_pbc.h
+++ FDTD/extensions/engine_ext_pbc.h
@@ -1,4 +1,24 @@
 #ifndef ENGINE_EXT_PBC_H
 #define ENGINE_EXT_PBC_H
 
+#include "engine_extension.h"
+#include "FDTD/engine.h"
+#include "FDTD/operator.h"
+
+class Operator_Ext_Pbc;
+
+class Engine_Ext_Pbc : public Engine_Extension
+{
+public:
+    Engine_Ext_Pbc(Operator_Ext_Pbc* op_ext);
+    virtual ~Engine_Ext_Pbc();
+
+    virtual void Apply2Voltages();
+    virtual void Apply2Current();
+
+protected:
+    Operator_Ext_Pbc* m_Op_Exc;
+};
+
+
 #endif // ENGINE_EXT_PBC_H
