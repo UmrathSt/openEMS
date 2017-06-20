f05f17e7be7e4c60382e44912a87868e34338c98
umra_st <stefan.umrath@dlr.de>
2017-06-20 08:01:46 +0200

    generated PBC operator extension from operator_ext_excitation as a template

--- FDTD/extensions/operator_ext_pbc.h
+++ FDTD/extensions/operator_ext_pbc.h
@@ -24,7 +24,7 @@ public:
     virtual bool IsCylindricalMultiGridSave(bool child) const {UNUSED(child); return true;}
     virtual bool IsMPISave() const {return true;}
 
-    virtual string GetExtensionName() const {return string("Excitation Extension");}
+    virtual string GetExtensionName() const {return string("PeriodicBoundaryCondition Extension");}
 
     virtual void ShowStat(ostream &ostr) const;
 
@@ -39,28 +39,6 @@ public:
 
 protected:
     Operator_Ext_Pbc(Operator* op, Operator_Ext_Pbc* op_ext);
-
-    Excitation* m_Exc;
-
-    void setupVoltageExcitation( vector<unsigned int> const volt_vIndex[3], vector<FDTD_FLOAT> const& volt_vExcit,
-                                 vector<unsigned int> const& volt_vDelay, vector<unsigned int> const& volt_vDir );
-    void setupCurrentExcitation( vector<unsigned int> const curr_vIndex[3], vector<FDTD_FLOAT> const& curr_vExcit,
-                                 vector<unsigned int> const& curr_vDelay, vector<unsigned int> const& curr_vDir );
-    //E-Field/voltage Excitation
-    unsigned int Volt_Count;
-    unsigned int Volt_Count_Dir[3];
-    unsigned int* Volt_index[3];
-    unsigned short* Volt_dir;
-    FDTD_FLOAT* Volt_amp; //represented as edge-voltages!!
-    unsigned int* Volt_delay;
-
-    //H-Field/current Excitation
-    unsigned int Curr_Count;
-    unsigned int Curr_Count_Dir[3];
-    unsigned int* Curr_index[3];
-    unsigned short* Curr_dir;
-    FDTD_FLOAT* Curr_amp; //represented as edge-currents!!
-    unsigned int* Curr_delay;
 };
 
 
