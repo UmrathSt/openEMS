102bcbd561a4825612c772d4df87c9ec71a313db
Stefan <Stefan.Umrath@dlr.de>
2017-07-31 13:57:50 +0200

    Adopted class variables from engine_ext_mur_abc.h

--- FDTD/extensions/engine_ext_pbc.h
+++ FDTD/extensions/engine_ext_pbc.h
@@ -1,3 +1,22 @@
+/*
+*	Copyright (C) 2010 Thorsten Liebig (Thorsten.Liebig@gmx.de)
+*   This module has been implemented by Stefan Umrath at the
+*   German Aerospace Center (DLR e. V., mail: Stefan.Umrath@dlr.de )
+*
+*	This program is free software: you can redistribute it and/or modify
+*	it under the terms of the GNU General Public License as published by
+*	the Free Software Foundation, either version 3 of the License, or
+*	(at your option) any later version.
+*
+*	This program is distributed in the hope that it will be useful,
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*	GNU General Public License for more details.
+*
+*	You should have received a copy of the GNU General Public License
+*	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef ENGINE_EXT_PBC_H
 #define ENGINE_EXT_PBC_H
 
@@ -13,12 +32,33 @@ public:
     Engine_Ext_Pbc(Operator_Ext_Pbc* op_ext);
     virtual ~Engine_Ext_Pbc();
 
-    virtual void Apply2Voltages();
-    virtual void Apply2Current();
+    virtual void SetNumberOfThreads(int nrThread);
+
+    virtual void DoPreVoltageUpdates() {Engine_Ext_Pbc::DoPreVoltageUpdates(0);}
+    virtual void DoPreVoltageUpdates(int threadID);
+    virtual void DoPostVoltageUpdates() {Engine_Ext_Pbc::DoPostVoltageUpdates(0);}
+    virtual void DoPostVoltageUpdates(int threadID);
+    virtual void Apply2Voltages() {Engine_Ext_Pbc::Apply2Voltages(0);}
+    virtual void Apply2Voltages(int threadID);
 
 protected:
     Operator_Ext_Pbc* m_Op_Pbc;
-};
+    inline bool IsActive() {if (m_Eng->GetNumberOfTimesteps()<m_start_TS) return false; return true;}
+    unsigned int m_start_TS;
 
+    int m_ny;
+    int m_nyP,m_nyPP;
+    unsigned int m_LineNr;
+    int m_LineNr_Shift;
+    unsigned int m_numLines[2];
+
+    vector<unsigned int> m_start;
+    vector<unsigned int> m_numX;
+
+    FDTD_FLOAT** m_Mur_Coeff_nyP;
+    FDTD_FLOAT** m_Mur_Coeff_nyPP;
+    FDTD_FLOAT** m_volt_nyP; //n+1 direction
+    FDTD_FLOAT** m_volt_nyPP; //n+2 direction
+};
 
 #endif // ENGINE_EXT_PBC_H
