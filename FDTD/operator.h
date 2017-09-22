458ad0d6bc47cc5a70314512cb8b54e3ed1096e9
umra_st <stefan.umrath@dlr.de>
2017-09-22 14:16:29 +0200

    Comments for changes to be applied for PBC

--- FDTD/operator.h
+++ FDTD/operator.h
@@ -39,6 +39,7 @@ class Operator : public Operator_Base
 	friend class Operator_Ext_Excitation;
 	friend class Operator_Ext_UPML;
 	friend class Operator_Ext_Cylinder;
+    // friend class Operator_Ext_Pbc;
 public:
 	enum DebugFlags {None=0,debugMaterial=1,debugOperator=2,debugPEC=4};
 
